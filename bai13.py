178d8992941dcd3f6a1ae548f4a719fa80e23ce2
phamhongphuc05 <phamhongphuc2021@gmail.com>
2024-08-30 22:24:40 +0700

    Add files via upload

---
+++ bai13.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 22:20:15 2024
+
+@author: kid30
+"""
+
+a = int(input("Nhập ngày: "))
+b = int(input("Nhập tháng: "))
+c = int(input("Nhập năm: "))
+print(f"{a}/{b}/{c}:")
+a = int(input("Nhập ngày: "))
+b = int(input("Nhập tháng: "))
+c = int(input("Nhập năm: "))
+t = c%100 
+print(f"{a}/{b}/{t}:")
+a = int(input("Nhập năm: "))
+b = int(input("Nhập tháng: "))
+c = int(input("Nhập ngày: "))
+print(f"{a}/{b}/{c}:")
